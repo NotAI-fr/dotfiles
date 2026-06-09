2922f46500ab6d6633b196d30cab233e6eeae203
NotAI-fr <runa313@proton.me>
2026-06-09 12:49:29 +0100

    Automated backup on 2026-06-09 12:49

--- Rofi/rofi/power-menu.sh
+++ Rofi/rofi/power-menu.sh
@@ -1,7 +1,21 @@
 #!/usr/bin/env bash
-chosen=$(echo -e "Shutdown\nReboot\nExit i3" | rofi -dmenu -theme ~/.config/rofi/mono.rasi -p "Power:")
+
+chosen=$(printf "󰐥 Shutdown\n󰜉 Reboot\n󰤄 Suspend\n󰌾 Lock\n󰍃 Logout\n" | rofi -dmenu -theme ~/.config/rofi/mono.rasi -p "Power")
+
 case "$chosen" in
-    Shutdown) poweroff ;;
-    Reboot) reboot ;;
-    "Exit i3") i3-msg exit ;;
+    "󰐥 Shutdown")
+        poweroff
+        ;;
+    "󰜉 Reboot")
+        reboot
+        ;;
+    "󰤄 Suspend")
+        systemctl suspend
+        ;;
+    "󰌾 Lock")
+        betterlockscreen -l
+        ;;
+    "󰍃 Logout")
+        i3-msg exit
+        ;;
 esac
