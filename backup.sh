9e5ba09fdab31fd0175b58762f0b04bb2608f127
NotAI-fr <runa313@proton.me>
2026-06-10 20:30:56 +0100

    Automated backup on 2026-06-10 20:30

--- backup.sh
+++ backup.sh
@@ -22,7 +22,7 @@ cp -r "$HOME/.config/mpv/" "$REPO_DIR/mpv/"
 cp -r "$HOME/.config/mpd/" "$REPO_DIR/mpd/"
 cp -r "$HOME/.zshrc" "$REPO_DIR/"
 cp -r "$HOME/.config/starship.toml" "$REPO_DIR/"
-cp -r "$HOME/.config/picom/ "$REPO_DIR/"picom/"
+cp -r "$HOME/.config/picom/" "$REPO_DIR/picom/"
 
 
 # You can add as many of these as you want later!
